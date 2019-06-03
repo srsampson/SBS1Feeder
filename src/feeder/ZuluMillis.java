40937cc9d0b8268645fe3b7d62a2483eee307c9f
Steve <coupaydeville@gmail.com>
2019-06-03 11:37:34 -0500

    Update ZuluMillis.java

--- src/feeder/ZuluMillis.java
+++ src/feeder/ZuluMillis.java
@@ -3,24 +3,17 @@
  */
 package feeder;
 
-import java.util.Calendar;
+mport java.util.Calendar;
 import java.util.GregorianCalendar;
 
-/**
- * Algorithm to give us Zulu Time.
- */
-public class ZuluMillis {
+public final class ZuluMillis {
 
-    private final Calendar cal;
-
-    public ZuluMillis() {
-        this.cal = new GregorianCalendar();
-    }
+    private static final Calendar cal = new GregorianCalendar();
 
     /**
-     * Method to return the current UTC time in milliseconds
+     * Method to return the current UTC time
      *
-     * @return a long Representing the time in UTC milliseconds
+     * @return a long Representing the UTC time.
      */
     public long getUTCTime() {
         cal.setTimeInMillis(System.currentTimeMillis());
