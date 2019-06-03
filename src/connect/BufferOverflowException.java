a6c5fc591b7147829d7a6093c38ad1b584012fb9
srsampson <coupaydeville@gmail.com>
2019-06-03 10:36:50 -0500

    Initial commit

--- src/connect/BufferOverflowException.java
+++ src/connect/BufferOverflowException.java
@@ -1,19 +1,6 @@
 /**
  * Buffer Overflow Exception Copyright (C) 2002-2010 Stephen Ostermiller
  * http://ostermiller.org/contact.pl?regarding=Java+Utilities
- *
- * <p>This program is free software: you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the Free
- * Software Foundation, either version 3 of the License, or (at your option) any
- * later version.
- *
- * <p>This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
- * details.
- *
- * <p>You should have received a copy of the GNU General Public License along
- * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 package connect;
 
